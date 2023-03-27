72167bb2bb7778db26a873ab3d24cb77ccb9ad5c
Ivankhomyk2000 <ivan.khomyk.work@gmail.com>
2023-03-27 13:26:27 +0300

    Create image

---
+++ Image/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+internal class Program
+{
+    static void Main()
+    {
+        Random rng = new Random();
+        byte[,,] colors = new byte[3, 1080, 1920];
+
+        for (int color = 0; color < colors.GetLength(0); color++)
+        {
+            for (int row = 0; row < colors.GetLength(1); row++)
+            {
+                for (int col = 0; col < colors.GetLength(2); col++)
+                {
+                    colors[color, row, col] = (byte)rng.Next(0, 255);
+                }
+
+            }
+        }
+
+        Bitmap bitMapImage = new Bitmap(colors.GetLength(2), colors.GetLength(1), PixelFormat.Format24bppRgb );
+        for (int row = 0; row < bitMapImage.Height; row++)
+        {
+            for (int col = 0; col < bitMapImage.Width; col++)
+            {
+                bitMapImage.SetPixel(col, row, Color.FromArgb(colors[0, row, col], colors[1, row, col], colors[2, row, col]));
+            }
+        }
+        bitMapImage.Save("test.png");
+    }
+}
+
---
+++ MultymationalArray/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+
+
+internal class Program
+{
+    static void Main()
+    {
+        Random rng = new Random();
+        int years = 3;
+        int months = 12;
+        int days = 31;
+        int hours = 24;
+        int[,,,] temperaures = new int[years, months, days, hours];
+
+        for (int year = 0; year < temperaures.GetLength(0); year++)
+        {
+            for (int month = 0; month < temperaures.GetLength(1); month++)
+            {
+                for (int day = 0; day < temperaures.GetLength(2); day++)
+                {
+                    for (int hour = 0; hour < temperaures.GetLength(3); hour++)
+                    {
+                        if (month >= 0 && month <= 2)
+                        {
+                            temperaures[year, month, day, hour] = rng.Next(-10, 15);
+                        }
+                        else if (month >= 3 && month <= 5)
+                        {
+                            temperaures[year, month, day, hour] = rng.Next(15, 25);
+                        }
+                        else if (month >= 6 && month <= 8)
+                        {
+                            temperaures[year, month, day, hour] = rng.Next(25, 35);
+                        }
+                        else if(month >= 9 && month <= 11)
+                        {
+                            temperaures[year, month, day, hour] = rng.Next(0, 25);
+                        }
+                    }
+                }
+            }
+        }
+        
+    }
+}
+
